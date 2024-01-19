069fad57c1113e0daa463ae8e6d02e368a456c8c
irawrr <ira-koval-2004@mail.ru>
2024-01-19 17:14:17 +1000

    fixed .cpp file

--- ComplexNumber.cpp
+++ ComplexNumber.cpp
@@ -1,97 +1,89 @@
 ﻿#include <iostream>
-#include <math>
 #include "ComplexNumber.h"
 
-class Complex
+
+Complex::Complex(double real, double imaginary) : real(real), imaginary(imaginary) {}
+
+double Complex::getReal() const { return real; }
+void Complex::setReal(double real) { this->real = real; }
+
+double Complex::getImaginary() const { return imaginary; }
+void Complex::setImaginary(double imaginary) { this->imaginary = imaginary; }
+
+Complex::Complex operator+(const Complex& other) const
 {
-private:
-    double real;
-    double imaginary;
+    return Complex(real + other.real, imaginary + other.imaginary);
+}
 
-public:
-    Complex::Complex(double real, double imaginary) : real(real), imaginary(imaginary) {}
+Complex::Complex operator-(const Complex& other) const
+{
+    return Complex(real - other.real, imaginary - other.imaginary);
+}
 
-    double Complex::getReal() const { return real; }
-    void Complex::setReal(double real) { this->real = real; }
+Complex::Complex operator*(const Complex& other) const
+{
+    double newReal = real * other.real - imaginary * other.imaginary;
+    double newImaginary = real * other.imaginary + imaginary * other.real;
+    return Complex(newReal, newImaginary);
+}
 
-    double Complex::getImaginary() const { return imaginary; }
-    void Complex::setImaginary(double imaginary) { this->imaginary = imaginary; }
+Complex::Complex operator/(const Complex& other) const
+{
+    double denominator = other.real * other.real + other.imaginary * other.imaginary;
+    double newReal = (real * other.real + imaginary * other.imaginary) / denominator;
+    double newImaginary = (imaginary * other.real - real * other.imaginary) / denominator;
+    return Complex(newReal, newImaginary);
+}
 
-    Complex::Complex operator+(const Complex& other) const
+bool Complex::operator==(const Complex& other) const
+{
+    return (real == other.real) && (imaginary == other.imaginary);
+}
+
+bool Complex::operator!=(const Complex& other) const
+{
+    return !(*this == other);
+}
+
+bool Complex::operator==(const double& number) const
+{
+    return (real == number) && (imaginary == 0);
+}
+
+bool Complex::operator!=(const double& number) const
+{
+    return !(*this == number);
+}
+
+double Complex::modulus() const
+{
+    return sqrt(real * real + imaginary * imaginary);
+}
+
+Complex::Complex power(int exponent) const
+{
+    double mod = modulus();
+    double arg = atan2(imaginary, real);
+    double newMod = pow(mod, exponent);
+    double newArg = arg * exponent;
+    double newReal = newMod * cos(newArg);
+    double newImaginary = newMod * sin(newArg);
+    return Complex(newReal, newImaginary);
+}
+
+friend std::ostream& operator<<(std::ostream& os, const Complex& complex)
+{
+    os << complex.real;
+    if (complex.imaginary >= 0)
     {
-        return Complex(real + other.real, imaginary + other.imaginary);
+        os << " + " << complex.imaginary << "i";
     }
-
-    Complex::Complex operator-(const Complex& other) const
+    else
     {
-        return Complex(real - other.real, imaginary - other.imaginary);
+        os << " - " << -complex.imaginary << "i";
     }
-
-    Complex::Complex operator*(const Complex& other) const
-    {
-        double newReal = real * other.real - imaginary * other.imaginary;
-        double newImaginary = real * other.imaginary + imaginary * other.real;
-        return Complex(newReal, newImaginary);
-    }
-
-    Complex::Complex operator/(const Complex& other) const
-    {
-        double denominator = other.real * other.real + other.imaginary * other.imaginary;
-        double newReal = (real * other.real + imaginary * other.imaginary) / denominator;
-        double newImaginary = (imaginary * other.real - real * other.imaginary) / denominator;
-        return Complex(newReal, newImaginary);
-    }
-
-    bool Complex::operator==(const Complex& other) const
-    {
-        return (real == other.real) && (imaginary == other.imaginary);
-    }
-
-    bool Complex::operator!=(const Complex& other) const
-    {
-        return !(*this == other);
-    }
-
-    bool Complex::operator==(const double& number) const
-    {
-        return (real == number) && (imaginary == 0);
-    }
-
-    bool Complex::operator!=(const double& number) const
-    {
-        return !(*this == number);
-    }
-
-    double Complex::modulus() const
-    {
-        return sqrt(real * real + imaginary * imaginary);
-    }
-
-    Complex::Complex power(int exponent) const
-    {
-        double mod = modulus();
-        double arg = atan2(imaginary, real);
-        double newMod = pow(mod, exponent);
-        double newArg = arg * exponent;
-        double newReal = newMod * cos(newArg);
-        double newImaginary = newMod * sin(newArg);
-        return Complex(newReal, newImaginary);
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const Complex& complex)
-    {
-        os << complex.real;
-        if (complex.imaginary >= 0)
-        {
-            os << " + " << complex.imaginary << "i";
-        }
-        else
-        {
-            os << " - " << -complex.imaginary << "i";
-        }
-        return os;
-    }
-};
+    return os;
+}
 
 int main()
 {
